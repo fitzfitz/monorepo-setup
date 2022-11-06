827eb95df6a660bb97a3001ed22929180d1adc71
Fitzgeral <fitzgeralmedia@gmail.com>
2022-11-06 08:10:58 +0700

    setup changelog and husky

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,6 +1,4 @@
 module.exports = {
-  // This tells ESLint to load the config from the package `eslint-config-custom`
-  // extends: ["@fitzzz/config/eslint-config-custom"],
   ...require("@fitzzz/config/eslint-config-custom"),
   root: true,
 };
--- packages/fitzzz-core/src/index.tsx
+++ packages/fitzzz-core/src/index.tsx
@@ -1,2 +1,2 @@
 import * as React from "react";
-export * from "./components/Button";
+export { Button, type ButtonProps } from "./components/Button";
